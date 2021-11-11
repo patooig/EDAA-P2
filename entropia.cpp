f826f494c21d7faf2808fee5011e1fd3a05935e4
patooig <patooig@gmail.com>
2021-11-11 16:12:14 -0300

    Cambios
    Co-authored-by: Leonardo Aravena Cuevas <ldaravena@users.noreply.github.com>

--- entropia.cpp
+++ entropia.cpp
@@ -1,16 +1,18 @@
 #include<bits/stdc++.h>
+#include<math.h>
 
 using namespace std;
 
 int main(){
 
     int c;
-
+    int contador;
     map<int,int> m;
+    map<int,int>::iterator it;
 
     while(cin>>c){
 
-
+        contador++;
         if(m.count(c)==0){
 
             m[c]=1;
@@ -22,7 +24,15 @@ int main(){
 
     }
 
-    cout<<m[27]<<endl;
+    //cout<<m[27]<<endl;
 
+    int h = 0;
+
+    for(it = m.begin(); it != m.end(); it++){
+
+        h += (it->second / contador) * log2(contador / it->second);
+    }
+
+    cout<<h<<endl;
     return 0;
 }
